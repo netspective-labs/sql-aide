ccfee1816001b32f5828e272b0caf6147e2cd917
Shahid N. Shah <shahid@shah.org>
2023-03-25 17:22:57 -0400

    feat: migrate PG dialect serial PK

---
+++ dialect/pg/primary-key.ts
@@ -0,0 +1,62 @@
+import { zod as z } from "../../deps.ts";
+import * as tbl from "../../ddl/table/mod.ts";
+import * as emit from "../../emit/mod.ts";
+import * as d from "../../domain/mod.ts";
+
+// deno-lint-ignore no-explicit-any
+type Any = any; // make it easy on linter
+
+export function primaryKeyColumnFactory<Context extends emit.SqlEmitContext>() {
+  const pkcf = tbl.primaryKeyColumnFactory<Context>();
+  const { sdFactory: sdf, zodBaggage: zb } = pkcf;
+
+  const serialPrimaryKey = <ColumnName extends string>() => {
+    const zodType = z.number().optional();
+    const sqlDomain: d.SqlDomain<
+      z.ZodOptional<z.ZodNumber>,
+      Context,
+      ColumnName
+    > = {
+      ...sdf.numberSDF.defaults<ColumnName>(zodType, { isOptional: true }),
+      sqlDataType: () => ({ SQL: () => `SERIAL` }),
+    };
+    const pkSD:
+      & tbl.TablePrimaryKeyColumnDefn<typeof zodType, Context>
+      & tbl.TableColumnInsertDmlExclusionSupplier<
+        z.ZodOptional<z.ZodNumber>,
+        Context
+      > = {
+        ...sqlDomain,
+        isPrimaryKey: true,
+        isExcludedFromInsertDML: true,
+        isAutoIncrement: true,
+        sqlPartial: (dest) => {
+          if (dest === "create table, column defn decorators") {
+            const ctcdd = sqlDomain?.sqlPartial?.(
+              "create table, column defn decorators",
+            );
+            const decorators: emit.SqlTextSupplier<Context> = {
+              SQL: () => `PRIMARY KEY`,
+            };
+            return ctcdd ? [decorators, ...ctcdd] : [decorators];
+          }
+          return sqlDomain.sqlPartial?.(dest);
+        },
+      };
+
+    // trick Typescript into thinking the Zod instance is also a SqlDomainSupplier;
+    // this allows assignment of a reference to a Zod object or use as a
+    // regular Zod schema; the sqlDomain is carried in zodType._def
+    // we do special typing of sqlDomain because isPrimaryKey, isExcludedFromInsertDML,
+    // etc. are needed by tableDefinition()
+    return zb.zodTypeBaggageProxy<typeof zodType>(
+      zodType,
+      pkSD,
+    ) as unknown as typeof zodType & { sqlDomain: typeof pkSD };
+  };
+
+  return {
+    ...pkcf,
+    serialPrimaryKey,
+  };
+}
