8c16f54256e6de13983d32ab14c92c3648a3ef5d
Shahid N. Shah <shahid@shah.org>
2023-05-11 16:45:03 -0400

    chore: configure for ESM-only,  prevent NPM errors

--- build-npm.ts
+++ build-npm.ts
@@ -1,10 +1,18 @@
-// ex. scripts/build_npm.ts
-import { build, emptyDir } from "https://deno.land/x/dnt/mod.ts";
+import { build, emptyDir } from "https://deno.land/x/dnt@0.35.0/mod.ts";
 
 await emptyDir("./npm");
 
 await build({
+  // TODO: turn off typeCheck while we figure out error:
+  //       src/deps/deno.land/x/eventemitter@1.2.4/mod.ts:218:4 - error TS2322: Type 'Timeout | null' is not assignable to type 'number | null'.
   typeCheck: false,
+
+  // disable type checking, testing, declaration emit, and CommonJS/UMD Output
+  // because we're targeting ESM
+  test: false,
+  declaration: false,
+  scriptModule: false,
+
   entryPoints: ["./render/mod.ts"],
   outDir: "./npm",
   shims: {
