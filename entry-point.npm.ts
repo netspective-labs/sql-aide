d49dfffe8c2dcb38fd5fa08584ddd6629b3c8c73
Manuel Casares <manuel.casares@mdevz.com>
2023-05-19 12:42:06 -0300

    chore: change some imports and exports

--- entry-point.npm.ts
+++ entry-point.npm.ts
@@ -1,8 +1,16 @@
-export * from "./render/domain/mod.ts";
-export * from "./render/dml/mod.ts";
-export * from "./render/ddl/mod.ts";
-export * from "./render/dql/mod.ts";
-export * from "./render/emit/mod.ts";
-export * from "./render/diagram/plantuml-ie-notation.ts";
 export * from "./pattern/data-vault.ts";
 export * from "./pattern/enum-table.ts";
+export * from "./render/mod.ts";
+
+// Default export
+import * as dataVault from "./pattern/data-vault.ts";
+import * as enumTable from "./pattern/enum-table.ts";
+import * as render from "./render/mod.ts";
+
+const defaultExport = {
+  ...dataVault,
+  ...enumTable,
+  ...render,
+};
+
+export default defaultExport;
--- render/mod.ts
+++ render/mod.ts
@@ -3,4 +3,5 @@ export * from "./dml/mod.ts";
 export * from "./ddl/mod.ts";
 export * from "./dql/mod.ts";
 export * from "./emit/mod.ts";
+export * from "./diagram/plantuml-ie-notation.ts";
 export * from "./graph.ts";
