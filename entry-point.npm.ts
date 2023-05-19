588e8d014c1c903599021b1f67dff6c2da43c178
Manuel Casares <manuel.casares@mdevz.com>
2023-05-19 10:26:31 -0300

    feat: added diagram/plantum to npm package

--- entry-point.npm.ts
+++ entry-point.npm.ts
@@ -3,5 +3,6 @@ export * from "./render/dml/mod.ts";
 export * from "./render/ddl/table/mod.ts";
 export * from "./render/dql/mod.ts";
 export * from "./render/emit/mod.ts";
+export * from "./render/diagram/plantuml-ie-notation.ts";
 export * from "./pattern/data-vault.ts";
 export * from "./pattern/enum-table.ts";
