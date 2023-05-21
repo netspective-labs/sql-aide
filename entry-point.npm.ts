cf6cb96f033e5f11d316372dafc4343db6b99a65
Shahid N. Shah <shahid@shah.org>
2023-05-21 18:34:28 -0400

    chore: fix location

--- entry-point.npm.ts
+++ entry-point.npm.ts
@@ -3,4 +3,4 @@ export * from "./render/dml/mod.ts";
 export * from "./render/ddl/table/mod.ts";
 export * from "./render/dql/mod.ts";
 export * from "./render/emit/mod.ts";
-export * from "./pattern/data-vault.ts";
+export * from "./pattern/data-vault/mod.ts";
