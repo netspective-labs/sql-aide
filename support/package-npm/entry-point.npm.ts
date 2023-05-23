7e5ec94f53f6943268ee6e378ffda5000ba720e2
Shahid N. Shah <shahid@shah.org>
2023-05-23 13:28:30 -0400

    refactor: move dnt packaging outside core

---
+++ support/package-npm/entry-point.npm.ts
@@ -0,0 +1,6 @@
+export * from "../../render/domain/mod.ts";
+export * from "../../render/dml/mod.ts";
+export * from "../../render/ddl/table/mod.ts";
+export * from "../../render/dql/mod.ts";
+export * from "../../render/emit/mod.ts";
+export * from "../../pattern/data-vault/mod.ts";
