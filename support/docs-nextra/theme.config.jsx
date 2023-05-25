70648852503bd38ac5fbcb49f99d58d0c84262a8
Shahid N. Shah <shahid@shah.org>
2023-05-24 21:45:10 -0400

    chore: update footer and deps

--- support/docs-nextra/theme.config.jsx
+++ support/docs-nextra/theme.config.jsx
@@ -2,7 +2,17 @@ export default {
     logo: <span>SQL Aide (SQLa) Documentation</span>,
     project: {
       link: 'https://github.com/netspective-labs/sql-aide'
+    },
+    footer: {
+      text: (
+        <span>
+          Copyright ©{' '} 1997-{new Date().getFullYear()} {' '}
+          <a href="https://github.com/netspective-labs" target="_blank">
+            Netspective Labs
+          </a>
+          . All rights reserved.
+        </span>
+      )
     }
-    // ...
   }
 
