b34d413e7753a409e2f2857f01d7dc54ab5ae7c5
Abdul Razak PM <razak@citrusinformatics.com>
2023-05-29 13:38:13 +0530

    fix: primary key for autoincrement in MSSQL   update primary-key.ts for primary key syntax   update sql table scripts   update sql test fixture

--- examples/jobs-mssql-sqla_test.fixture-01.sql
+++ examples/jobs-mssql-sqla_test.fixture-01.sql
@@ -6,9 +6,15 @@ CREATE TABLE [execution_context] (
 INSERT INTO [execution_context] ([code], [value]) VALUES (0, 'DEVELOPMENT');
 INSERT INTO [execution_context] ([code], [value]) VALUES (1, 'TEST');
 INSERT INTO [execution_context] ([code], [value]) VALUES (2, 'PRODUCTION');
-
+CREATE TABLE [job_grade] (
+    [job_grade_id] INTEGER IDENTITY(1,1) PRIMARY KEY,
+    [grade_name] TEXT NOT NULL,
+    [description] TEXT,
+    [created_at] DATETIME2 DEFAULT GETDATE(),
+    [created_by] TEXT DEFAULT 'UNKNOWN'
+);
 CREATE TABLE [job_position] (
-    [job_position_id] INTEGER IDENTITY(1,1) NOT NULL,
+    [job_position_id] INTEGER IDENTITY(1,1) PRIMARY KEY,
     [position_title] TEXT NOT NULL,
     [job_category_id] INTEGER NOT NULL,
     [description] TEXT,
@@ -27,14 +33,6 @@ CREATE TABLE [job_position] (
     [question_answers] TEXT,
     [official_id] INTEGER NOT NULL,
     [status_id] INTEGER NOT NULL,
-    [created_at] DATETIME2 DEFAULT GETDATE(),
-    [created_by] TEXT DEFAULT 'UNKNOWN'
-);
-
-CREATE TABLE [job_grade] (
-    [job_grade_id] INTEGER IDENTITY(1,1) NOT NULL,
-    [grade_name] TEXT NOT NULL,
-    [description] TEXT,
     [created_at] DATETIME2 DEFAULT GETDATE(),
     [created_by] TEXT DEFAULT 'UNKNOWN'
 );
