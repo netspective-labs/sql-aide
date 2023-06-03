8a73e7430b4e7de60c6b68304df144d86c3ea0ec
Ovais Butt <ovaisbutt786@gmail.com>
2023-06-03 07:40:03 +0500

    Added base path and assetprefix

--- support/docs-nextra/next.config.js
+++ support/docs-nextra/next.config.js
@@ -2,10 +2,12 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
 });
-
+const isProd = process.env.NODE_ENV === "production";
 module.exports = withNextra({
   // output: "export",
   trailingSlash: true,
+  basePath: isProd ? "/sql-aide" : undefined,
+  assetPrefix: isProd ? "/sql-aide" : undefined,
   // distDir: "../dist",
   images: {
     unoptimized: true,
