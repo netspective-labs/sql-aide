6a01c87fda5177c01c621e0a063565697f85c866
Shahid N. Shah <shahid@shah.org>
2023-06-03 22:18:48 -0400

    chore: update deps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export * as events from "https://deno.land/x/eventemitter@1.2.4/mod.ts";
-export * as fs from "https://deno.land/std@0.178.0/fs/mod.ts";
-export * as path from "https://deno.land/std@0.178.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.190.0/fs/mod.ts";
+export * as path from "https://deno.land/std@0.190.0/path/mod.ts";
 export * as zod from "https://deno.land/x/zod@v3.21.4/mod.ts";
