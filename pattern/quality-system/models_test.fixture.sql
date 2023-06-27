e8f07bf4b77303d3322be46119e07b66c873880b
Vinod A <vinod.aravindakshan@citrusinformatics.com>
2023-06-27 17:48:45 +0530

    feat: update quality system  #104
    update models wiith table and column comments
    add test fixtures files

---
+++ pattern/quality-system/models_test.fixture.sql
@@ -0,0 +1,129 @@
+PRAGMA foreign_keys = on; -- check foreign key reference, slightly worst performance
+
+-- reference tables
+CREATE TABLE IF NOT EXISTS "lineage_source_type" (
+    "lineage_source_type_id" TEXT PRIMARY KEY NOT NULL,
+    "lineage_source_type" TEXT NOT NULL,
+    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "created_by" TEXT DEFAULT 'UNKNOWN',
+    "updated_at" TIMESTAMP,
+    "updated_by" TEXT,
+    "deleted_at" TIMESTAMP,
+    "deleted_by" TEXT,
+    "activity_log" JSONB
+);
+CREATE TABLE IF NOT EXISTS "lineage_destination_type" (
+    "lineage_destination_type_id" TEXT PRIMARY KEY NOT NULL,
+    "lineage_destination_type" TEXT NOT NULL,
+    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "created_by" TEXT DEFAULT 'UNKNOWN',
+    "updated_at" TIMESTAMP,
+    "updated_by" TEXT,
+    "deleted_at" TIMESTAMP,
+    "deleted_by" TEXT,
+    "activity_log" JSONB
+);
+CREATE TABLE IF NOT EXISTS "lineage_transform_type" (
+    "lineage_transform_type_id" TEXT PRIMARY KEY NOT NULL,
+    "lineage_transform_type" TEXT NOT NULL,
+    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "created_by" TEXT DEFAULT 'UNKNOWN',
+    "updated_at" TIMESTAMP,
+    "updated_by" TEXT,
+    "deleted_at" TIMESTAMP,
+    "deleted_by" TEXT,
+    "activity_log" JSONB
+);
+
+-- content tables
+-- TODO:SqlObjectComment
+CREATE TABLE IF NOT EXISTS "lineage_source" (
+    "lineage_source_id" SERIAL PRIMARY KEY,
+    "lineage_source_type_id" TEXT NOT NULL,
+    "source_name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "created_by" TEXT DEFAULT 'UNKNOWN',
+    "updated_at" TIMESTAMP,
+    "updated_by" TEXT,
+    "deleted_at" TIMESTAMP,
+    "deleted_by" TEXT,
+    "activity_log" JSONB,
+    FOREIGN KEY("lineage_source_type_id") REFERENCES "lineage_source_type"("lineage_source_type_id")
+);
+CREATE TABLE IF NOT EXISTS "lineage_destination" (
+    "lineage_destination_id" SERIAL PRIMARY KEY,
+    "lineage_dest_type_id" TEXT NOT NULL,
+    "dest_name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "created_by" TEXT DEFAULT 'UNKNOWN',
+    "updated_at" TIMESTAMP,
+    "updated_by" TEXT,
+    "deleted_at" TIMESTAMP,
+    "deleted_by" TEXT,
+    "activity_log" JSONB,
+    FOREIGN KEY("lineage_dest_type_id") REFERENCES "lineage_destination_type"("lineage_destination_type_id")
+);
+CREATE TABLE IF NOT EXISTS "lineage_transform" (
+    "lineage_transform_id" INTEGER,
+    "lineage_transform_type_id" TEXT NOT NULL,
+    "transform_name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "created_by" TEXT DEFAULT 'UNKNOWN',
+    "updated_at" TIMESTAMP,
+    "updated_by" TEXT,
+    "deleted_at" TIMESTAMP,
+    "deleted_by" TEXT,
+    "activity_log" JSONB,
+    FOREIGN KEY("lineage_transform_type_id") REFERENCES "lineage_transform_type"("lineage_transform_type_id")
+);
+CREATE TABLE IF NOT EXISTS "lineage_graph_node" (
+    "lineage_graph_node_id" INTEGER,
+    "lineage_source_id" INTEGER NOT NULL,
+    "lineage_dest_id" INTEGER NOT NULL,
+    "lineage_transform_id" INTEGER NOT NULL,
+    "graph_node_name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "created_by" TEXT DEFAULT 'UNKNOWN',
+    "updated_at" TIMESTAMP,
+    "updated_by" TEXT,
+    "deleted_at" TIMESTAMP,
+    "deleted_by" TEXT,
+    "activity_log" JSONB,
+    FOREIGN KEY("lineage_source_id") REFERENCES "lineage_source"("lineage_source_id"),
+    FOREIGN KEY("lineage_dest_id") REFERENCES "lineage_destination"("lineage_destination_id"),
+    FOREIGN KEY("lineage_transform_id") REFERENCES "lineage_transform"("lineage_transform_id")
+);
+
+COMMENT ON column "lineage_source_type"."activity_log" IS '{"isSqlDomainZodDescrMeta":true,"isJsonSqlDomain":true}';
+COMMENT ON column "lineage_destination_type"."activity_log" IS '{"isSqlDomainZodDescrMeta":true,"isJsonSqlDomain":true}';
+COMMENT ON column "lineage_transform_type"."activity_log" IS '{"isSqlDomainZodDescrMeta":true,"isJsonSqlDomain":true}';
+COMMENT ON table "lineage_source" IS 'Entity representing sources of data transformations.';
+COMMENT ON column "lineage_source"."source_name" IS 'The name of the data source from which the lineage originates.';
+COMMENT ON column "lineage_source"."description" IS 'Description or additional information about the data source.';
+COMMENT ON column "lineage_source"."activity_log" IS '{"isSqlDomainZodDescrMeta":true,"isJsonSqlDomain":true}';
+COMMENT ON table "lineage_destination" IS 'Entity representing destinations of data transformations.';
+COMMENT ON column "lineage_destination"."dest_name" IS 'The name of the data destination to the lineage target.';
+COMMENT ON column "lineage_destination"."description" IS 'Description or additional information about the data target.';
+COMMENT ON column "lineage_destination"."activity_log" IS '{"isSqlDomainZodDescrMeta":true,"isJsonSqlDomain":true}';
+COMMENT ON table "lineage_transform" IS 'Entity representing transformations of data transformations.';
+COMMENT ON column "lineage_transform"."transform_name" IS 'The name of the data transormation';
+COMMENT ON column "lineage_transform"."description" IS 'Description of the transformation';
+COMMENT ON column "lineage_transform"."activity_log" IS '{"isSqlDomainZodDescrMeta":true,"isJsonSqlDomain":true}';
+COMMENT ON table "lineage_graph_node" IS 'Entity representing a single node of the lineage graph.';
+COMMENT ON column "lineage_graph_node"."lineage_source_id" IS 'Foreign key referencing the lineage_source_id column in the lineage_source table.';
+COMMENT ON column "lineage_graph_node"."lineage_dest_id" IS 'Foreign key referencing the lineage_dest_id column in the lineage_target table.';
+COMMENT ON column "lineage_graph_node"."lineage_transform_id" IS 'Foreign key referencing the lineage_transform_id column in the lineage_transform table.';
+COMMENT ON column "lineage_graph_node"."description" IS 'description or additional information about the node of the lineage graph';
+COMMENT ON column "lineage_graph_node"."activity_log" IS '{"isSqlDomainZodDescrMeta":true,"isJsonSqlDomain":true}';
+
+
+--content views
+-- {allContentViews}
+
+-- seed Data
+-- {allReferenceTables.map(e => e.seedDML).flat()}
+;
---
+++ pattern/quality-system/models_test.ts
@@ -0,0 +1,130 @@
+#!/usr/bin/env -S deno run --allow-all
+
+import $ from "https://deno.land/x/dax@0.30.1/mod.ts";
+import { testingAsserts as ta } from "../../deps-test.ts";
+import * as ws from "../../lib/universal/whitespace.ts";
+import * as SQLa from "../../render/mod.ts";
+import * as tp from "../typical/mod.ts";
+import * as mod from "./models.ts";
+
+const relativeFilePath = (name: string) => {
+  const absPath = $.path.fromFileUrl(import.meta.resolve(name));
+  return $.path.relative(Deno.cwd(), absPath);
+};
+
+const relativeFileContent = (name: string) => {
+  const absPath = $.path.fromFileUrl(import.meta.resolve(name));
+  return Deno.readTextFileSync($.path.relative(Deno.cwd(), absPath));
+};
+
+type EmitContext = typeof ctx;
+const ctx = SQLa.typicalSqlEmitContext({
+  sqlDialect: SQLa.postgreSqlDialect(),
+});
+const gts = tp.governedTemplateState<
+  tp.TypicalDomainQS,
+  tp.TypicalDomainsQS,
+  EmitContext
+>();
+
+function sqlDDL() {
+  return SQLa.SQL<EmitContext>(gts.ddlOptions)`
+    ${mod.sqlDDL()}
+    `;
+}
+
+if (import.meta.main) {
+  await tp.typicalCLI({
+    resolve: (specifier) =>
+      specifier ? import.meta.resolve(specifier) : import.meta.url,
+    prepareSQL: () => ws.unindentWhitespace(sqlDDL().SQL(ctx)),
+    prepareDiagram: () => {
+      return tp.diaPUML.plantUmlIE(ctx, function* () {
+        for (const table of mod.allContentTables) {
+          if (SQLa.isGraphEntityDefinitionSupplier(table)) {
+            yield table.graphEntityDefn();
+          }
+        }
+      }, tp.diaPUML.typicalPlantUmlIeOptions()).content;
+    },
+  }).commands.command("driver", tp.sqliteDriverCommand(sqlDDL, ctx)).command(
+    "test-fixtures",
+    new tp.cli.Command()
+      .description("Emit all test fixtures")
+      .action(async () => {
+        const CLI = relativeFilePath("./models_test.ts");
+        const [sql, puml, sh] = [".sql", ".puml", ".sh"].map((extn) =>
+          relativeFilePath(`./models_test.fixture${extn}`)
+        );
+        Deno.writeTextFileSync(sql, await $`./${CLI} sql`.text());
+        Deno.writeTextFileSync(puml, await $`./${CLI} diagram`.text());
+        Deno.writeTextFileSync(sh, await $`./${CLI} driver`.text());
+        [sql, puml, sh].forEach((f) => console.log(f));
+      }),
+  ).parse(Deno.args);
+}
+
+/**
+ * This is an "end-to-end" test strategy; we generate our fixtures whenever
+ * our information model (schema) changes and git-track those files so that
+ * if SQLa or other library changes impact what's generated we'll know because
+ * the Deno test will fail.
+ *
+ * to re-generate all fixtures
+ * $ ./models_test.ts test-fixtures
+ *
+ * to re-generate the fixtures one at a time:
+ * $ ./models_test.ts sql --dest models_test.fixture.sql
+ * $ ./models_test.ts diagram --dest models_test.fixture.puml
+ * $ ./models_test.ts driver --dest ./models_test.fixture.sh && chmod +x ./models_test.fixture.sh
+ */
+Deno.test("Information Assurance Pattern", async (tc) => {
+  const CLI = relativeFilePath("./models_test.ts");
+
+  await tc.step("CLI SQL content", async () => {
+    const output = await $`./${CLI} sql`.text();
+    ta.assertEquals(
+      output,
+      relativeFileContent("./models_test.fixture.sql"),
+    );
+  });
+
+  await tc.step("CLI diagram", async () => {
+    const output = await $`./${CLI} diagram`.text();
+    ta.assertEquals(
+      output,
+      relativeFileContent("./models_test.fixture.puml"),
+    );
+  });
+
+  await tc.step("CLI driver content", async () => {
+    const output = await $`./${CLI} driver`.text();
+    ta.assertEquals(
+      output,
+      relativeFileContent("./models_test.fixture.sh"),
+    );
+  });
+
+  /**
+   * Execute the "driver" so that it creates an in-memory SQLite database and
+   * returns the total number of objects found in the SQLite ephemeral DB. If
+   * the count is equivalent to our expectation it means everything worked.
+   */
+  await tc.step("CLI driver execution result", async () => {
+    const sh = relativeFilePath("./models_test.fixture.sh");
+    // TODO: right now we just check the total count of object but this should be
+    // improved to actually check the names of each table, view, etc.
+    // deno-fmt-ignore
+    const output = await $`./${sh} :memory: "select count(*) as objects_count from sqlite_master"`.text();
+    ta.assertEquals(output, "12");
+  });
+
+  // deno-lint-ignore require-await
+  await tc.step("Typescript SQL", async () => {
+    const output = sqlDDL().SQL(ctx);
+    ta.assertEquals(
+      output,
+      relativeFileContent("./models_test.fixture.sql"),
+    );
+  });
+});
