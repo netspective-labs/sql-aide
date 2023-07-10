661ca8fef27ee19b3351081919c6695d4b124a5c
Shahid N. Shah <shahid@shah.org>
2023-07-10 10:28:13 -0400

    fix: introduce bash script generator #37

--- lib/sqlite/mod-flow.ts
+++ lib/sqlite/mod-flow.ts
@@ -1,10 +1,14 @@
-#!/usr/bin/env -S deno run --allow-read
+#!/usr/bin/env -S deno run --allow-read --allow-write
 
 import * as sh from "./shell.ts";
 import * as flow from "../flow/mod.ts";
 import * as mod from "./mod.ts";
 
-export function flowCLI(args?: Parameters<typeof flow.cliInfrastructure>[0]) {
+export function flowCLI<SqlIdentity extends string>(
+  args?: Parameters<typeof flow.cliInfrastructure>[0] & {
+    library?: () => Record<SqlIdentity, mod.SqlTextSupplier>;
+  },
+) {
   const clii = flow.cliInfrastructure({
     ...args,
     resolveURI: (specifier) =>
@@ -25,6 +29,19 @@ export function flowCLI(args?: Parameters<typeof flow.cliInfrastructure>[0]) {
             options.dest,
             { fileSystemPath: options.sqlSrc },
             { ...options, report: clii.reportCLI(options) })))
+      .command("bash", clii.command()
+        .description("generate a bash script for driving SQLite database")
+        .option("--dest=<name>", "the destination of the bash script, defaults to STDOUT")
+        .option("--sql-src=<SQL>", "the src of SQL that should be passed into destination", { required: true })
+        .action(
+           async (options) => {
+              const scriptContent = sh.sqliteMemToFileBashScript({ fileSystemPath: options.sqlSrc }, options.dest);
+              if(options.dest) {
+                await Deno.writeTextFile(options.dest, scriptContent);
+              } else {
+                console.log(scriptContent);
+              }
+           }))
       .command("sql", clii.command()
         .command("inspect", clii.command()
           .description("Emit SQLite schema inspection SQL")
@@ -39,27 +56,29 @@ export function flowCLI(args?: Parameters<typeof flow.cliInfrastructure>[0]) {
           .option("-s, --separators", "include comments before each SQL block")
           .arguments("[identity...]")
           .action(({ separators, list }, ...identities) => {
-            const library = mod.sqlLibrary;
+            const library = args?.library?.();
+            if(!library) return;
             if(list) {
               Array.from(Object.keys(library)).forEach(k => console.log(k))
             } else {
               if(identities.length > 0) {
                 for(const key of identities) {
                   if(key in library) {
-                    console.log(`${separators ? `-- ${key}\n` : ''}${library[key as mod.SqlIdentity].SQL(mod.ctx)}\n`);
+                    console.log(`${separators ? `-- ${key}\n` : ''}${library[key as SqlIdentity].SQL(mod.ctx)}\n`);
                   }
                 }
               } else {
-                for(const [key, value] of Object.entries(library)) {
+                for(const [key, value] of Object.entries<mod.SqlTextSupplier>(library)) {
                   console.log(`${separators ? `-- ${key}\n` : ''}${value.SQL(mod.ctx)}\n`);
                 }
               }
             }
-          }))).description('emit SQLite SQL'),
+          })))
+        .description('emit SQLite SQL'),
   };
 }
 
 if (import.meta.main) {
-  const CLI = flowCLI({ cliName: "sqlite" });
+  const CLI = flowCLI({ cliName: "sqlite", library: () => mod.sqlLibrary });
   await CLI.commands.parse();
 }
--- lib/sqlite/shell.ts
+++ lib/sqlite/shell.ts
@@ -226,3 +226,90 @@ export async function executeSqliteUA(
     }
   }
 }
+
+/**
+ * Wrap the provided SQL into a SQLite bash script which will first load the
+ * file into an in-memory SQLite database then dump that SQL into a file-based
+ * SQLite database. We do the interim step because complex SQL will load
+ * faster with this two-step process. This is especially in SQLite because
+ * insert DMLs often use something like this, note `SELECT` for values.
+ *
+ * INSERT INTO "party_relation" ("party_id", "related_party_id", "relation_type_id", "party_role_id", "created_by")
+ *      VALUES ((SELECT "party_id" FROM "party" WHERE "party_type_id" = 'PERSON' AND "party_name" = 'person'),
+ *              (SELECT "party_id" FROM "party" WHERE "party_type_id" = 'PERSON' AND "party_name" = 'person'),
+ *              'ORGANIZATION_TO_PERSON', 'VENDOR', NULL);
+ *
+ * Because SQLite does not have any scripting language, if we want to use pure
+ * SQL we must "lookup" references as select statements. When those select
+ * statements are looked up in memory they're very fast but on disk it's slow.
+ * So, we run the first step in memory and then emit the output to disk.
+ *
+ * @param ss SQL supplier
+ * @param ctx active Context
+ * @returns text suitable for emitting to STDOUT or saving a text file
+ */
+export function sqliteMemToFileBashScript(
+  ss: FlexibleSqlTextSupplierSync,
+  scriptName = "driver.sh",
+) {
+  const unindent = (indentation: string, text: string): string =>
+    text.replace(new RegExp(`^${indentation}`, "gm"), "");
+
+  const indent = (text: string, spaces: string | number): string => {
+    const indentation = typeof spaces === "string"
+      ? spaces
+      : " ".repeat(spaces);
+    return text.replace(/^/gm, indentation);
+  };
+
+  // deno-fmt-ignore
+  return unindent("    ", `
+    #!/bin/bash
+
+    destroy_first=0
+    db_file=""
+
+    # Parse command-line options
+    for arg in "$@"
+    do
+        case $arg in
+            --destroy-first)
+                destroy_first=1
+                shift # Remove --destroy-first from processing
+                ;;
+            *)
+                db_file=$1
+                shift # Remove database filename from processing
+                break # Stop processing after the filename so we can pass the rest into final SQLite DB
+                ;;
+        esac
+    done
+
+    # Check if the database file parameter is supplied
+    if [ -z "$db_file" ]
+    then
+        echo "No database file supplied. Usage: ./${scriptName} [--destroy-first] <database_file> [<sqlite3 arguments>...]"
+        exit 1
+    fi
+
+    # If the destroy_first flag is set, delete the database file
+    if [ $destroy_first -eq 1 ] && [ -f "$db_file" ]; then
+        rm "$db_file"
+    fi
+
+    SQL=$(cat <<-EOF
+    ${/* we indent because there's an outer `unindent` */ sqlTextList(ss).map(SQL => indent(SQL, "    "))}
+    -- the .dump in the last line is necessary because we load into :memory:
+    -- first because performance is better and then emit all the SQL for saving
+    -- into the destination file, e.g. when insert DML uses (select x from y where a = b))
+    .dump
+    EOF
+    )
+
+    # Create an in-memory SQLite database, load the first pass for optimal
+    # performance then export the in-memory database to the given file; this
+    # two phase approach works because the last line in the SQL is '.dump'.
+    # All arguments after <database_file> will be passed into the final DB.
+    sqlite3 "$db_file" "$(echo "$SQL" | sqlite3 ":memory:")" "\${@}"
+  `);
+}
